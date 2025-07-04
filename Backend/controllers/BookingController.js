6efc5421644529685a81080008ee910c690a9ed2
Your Name <your-email@example.com>
2025-07-04 20:18:23 +0500

    Ongoing-Backend

--- Backend/controllers/BookingController.js
+++ Backend/controllers/BookingController.js
@@ -41,7 +41,7 @@ export const getUserBooking=async(req,res)=>{
  try{
        const {userId}=req.body
 
-    const UserBookings=await BookingModel.find(userId);
+    const UserBookings=await BookingModel.find({ userId });
 
     res.status(201).json({ success: true,UserBookings});
  }
@@ -50,3 +50,45 @@ export const getUserBooking=async(req,res)=>{
  }
 
 }
+
+export const getAdminBooking=async(req,res)=>{
+
+ try{
+       const {ownerId}=req.body
+
+    const OwnerBookings=await BookingModel.find({ ownerId });
+
+    res.status(201).json({ success: true,OwnerBookings});
+ }
+ catch(error){
+    res.status(500).json({ success: false, message: error.message });
+ }
+
+}
+
+
+
+export const changeStatus = async (req, res) => {
+  try {
+    const { id, status } = req.body;
+
+    if (!id || !status) {
+      return res.status(400).json({ success: false, message: "ID and status are required" });
+    }
+
+    const updatedStatus = await BookingModel.findByIdAndUpdate(
+      id,
+      { status },
+      { new: true } // return updated doc
+    );
+
+    if (!updatedStatus) {
+      return res.status(404).json({ success: false, message: "Booking not found" });
+    }
+
+    res.status(200).json({ success: true, updatedStatus });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+
--- Backend/routes/BookingRoute.js
+++ Backend/routes/BookingRoute.js
@@ -1,6 +1,9 @@
 import express from 'express';
-import { BookingCar } from '../controllers/BookingController.js';
+import { BookingCar, changeStatus, getAdminBooking, getUserBooking } from '../controllers/BookingController.js';
 const BookRouter=express.Router();
 
 BookRouter.post("/bookcar",BookingCar);
+BookRouter.get("/getuser",getUserBooking);
+BookRouter.get("/getowner",getAdminBooking);
+BookRouter.put("/status",changeStatus);
 export default BookRouter;
